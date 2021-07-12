37633728514de02c8ff485b0a9e3858f288c1f8c
Hyunwoo Choi <micael53@gmail.com>
2021-07-12 13:49:48 +0900

    Update signin2.js

--- pages/signin2.js
+++ pages/signin2.js
@@ -17,7 +17,7 @@ export default function SignIn() {
 
     try {
       await mutateUser(
-        fetchJson('/api/signin', {
+        fetchJson('/api/signin2', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(body),
